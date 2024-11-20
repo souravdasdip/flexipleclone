d5fdcc18c5a329a56a6849df676c21cd7877901d
Sourav Das <souravdasdip65@gmail.com>
2024-11-20 14:41:59 +0600

    Update rightbar.tsx

--- app/(home)/_components/rightbar.tsx
+++ app/(home)/_components/rightbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Rightbar = () => {
   return (
-    <div className="rounded-lg bg-custom-border-gradient w-full h-[350px] z-[99999] sticky top-0 grid place-items-center">
+    <div className="rounded-lg bg-custom-border-gradient w-full h-[350px] z-[99999] sticky top-2 grid place-items-center">
       <div className="p-8 flex flex-col gap-6 flex-1 w-[99%] h-[99%] bg-[#0C0B0F] rounded-lg">
         <h1 className="text-2xl">
           99% of Flexiple&apos;s clients hire talent after a risk-free trial.
