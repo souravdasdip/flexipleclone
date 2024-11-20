8688ff71567e49f6fb27c5a9eab78c5a08e4a535
Sourav Das <souravdasdip65@gmail.com>
2024-11-20 14:41:29 +0600

    Update rightbar.tsx

--- app/(home)/_components/rightbar.tsx
+++ app/(home)/_components/rightbar.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import React from "react";
 
 const Rightbar = () => {
   return (
-    <div className="rounded-lg bg-custom-border-gradient w-full h-auto z-[99999] sticky top-0">
-      <div className="p-8 flex flex-col gap-6 flex-1">
+    <div className="rounded-lg bg-custom-border-gradient w-full h-[350px] z-[99999] sticky top-0 grid place-items-center">
+      <div className="p-8 flex flex-col gap-6 flex-1 w-[99%] h-[99%] bg-[#0C0B0F] rounded-lg">
         <h1 className="text-2xl">
           99% of Flexiple&apos;s clients hire talent after a risk-free trial.
         </h1>
@@ -14,7 +15,9 @@ const Rightbar = () => {
           with exceptional developers perfectly aligned to your specific needs.
         </p>
 
-        <Button>Book a consultation call -&gt;</Button>
+        <Button>
+          Book a consultation call <ArrowRight />
+        </Button>
       </div>
     </div>
   );
